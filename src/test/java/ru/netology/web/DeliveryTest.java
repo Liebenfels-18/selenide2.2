1bdec4fe176062521cab514da7f79cbbaff53993
Liebenfels <nik.yarych2001@yandex.ru>
2024-02-28 00:01:16 +0300

    corrected remarks

--- src/test/java/ru/netology/web/DeliveryTest.java
+++ src/test/java/ru/netology/web/DeliveryTest.java
@@ -2,7 +2,9 @@ package ru.netology.web;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SetValueOptions;
+import com.codeborne.selenide.conditions.Text;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
 import java.time.Duration;
 import java.time.LocalDate;
@@ -10,27 +12,32 @@ import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.openqa.selenium.remote.tracing.EventAttribute.setValue;
 
+
 class DeliveryTest {
-    @Test
-    void shouldTestCardDeliveryCorrectFields() {
+
+    public String addDays(int daysToAdd) {
         DateTimeFormatter pattern = DateTimeFormatter.ofPattern("dd.MM.yyyy");
         LocalDate date = LocalDate.now();
-        LocalDate returnValue = date.plusDays(3);
-        String formattedDate = returnValue.format(pattern);
+        LocalDate returnValue = date.plusDays(daysToAdd);
+        return returnValue.format(pattern);
+    }
+    @Test
+    void shouldTestCardDeliveryCorrectFields() {
+
         open("http://localhost:9999/");
         $("[data-test-id='city'] .input__control").sendKeys("Москва");
         $("[data-test-id='date'] .input__control").doubleClick();
-        $("[data-test-id='date'] .input__control").sendKeys(" ");
-        $("[data-test-id='date'] .input__control").sendKeys(formattedDate);
+        $("[data-test-id='date'] .input__control").sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id='date'] .input__control").sendKeys(addDays(3));
         $("[data-test-id='name'] .input__control").sendKeys("Иван Иванов");
         $("[data-test-id='phone'] .input__control").sendKeys("+79998887766");
         $("[data-test-id='agreement']").click();
         $(byText("Забронировать")).click();
         $("[data-test-id='notification']").shouldBe(Condition.visible, Duration.ofSeconds(15));
+        $("[data-test-id='notification']").shouldHave(Condition.text("Встреча успешно забронирована на " + $("[data-test-id='date'] .input__control").text()));
     }
 
     @Test
@@ -42,14 +49,13 @@ class DeliveryTest {
         open("http://localhost:9999/");
         $("[data-test-id='city'] .input__control").sendKeys("Белая Калитва");
         $("[data-test-id='date'] .input__control").doubleClick();
-        $("[data-test-id='date'] .input__control").sendKeys(" ");
-        $("[data-test-id='date'] .input__control").sendKeys(formattedDate);
+        $("[data-test-id='date'] .input__control").sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id='date'] .input__control").sendKeys(addDays(3));
         $("[data-test-id='name'] .input__control").sendKeys("Иван Иванов");
         $("[data-test-id='phone'] .input__control").sendKeys("+79998887766");
         $("[data-test-id='agreement']").click();
         $(byText("Забронировать")).click();
-        String text = $("[data-test-id='city'].input_invalid .input__sub").getText();
-        assertEquals("Доставка в выбранный город недоступна", text);
+        $("[data-test-id='city'].input_invalid .input__sub").shouldHave(Condition.text("Доставка в выбранный город недоступна"));
     }
 
     @Test
@@ -61,14 +67,13 @@ class DeliveryTest {
         open("http://localhost:9999/");
         $("[data-test-id='city'] .input__control").sendKeys(" ");
         $("[data-test-id='date'] .input__control").doubleClick();
-        $("[data-test-id='date'] .input__control").sendKeys(" ");
-        $("[data-test-id='date'] .input__control").sendKeys(formattedDate);
+        $("[data-test-id='date'] .input__control").sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id='date'] .input__control").sendKeys(addDays(3));
         $("[data-test-id='name'] .input__control").sendKeys("Иван Иванов");
         $("[data-test-id='phone'] .input__control").sendKeys("+79998887766");
         $("[data-test-id='agreement']").click();
         $(byText("Забронировать")).click();
-        String text = $("[data-test-id='city'].input_invalid .input__sub").getText();
-        assertEquals("Поле обязательно для заполнения", text);
+        $("[data-test-id='city'].input_invalid .input__sub").shouldHave(Condition.text("Поле обязательно для заполнения"));
     }
 
     @Test
@@ -76,14 +81,13 @@ class DeliveryTest {
         open("http://localhost:9999/");
         $("[data-test-id='city'] .input__control").sendKeys("Ростов-на-Дону");
         $("[data-test-id='date'] .input__control").doubleClick();
-        $("[data-test-id='date'] .input__control").sendKeys(" ");
+        $("[data-test-id='date'] .input__control").sendKeys(Keys.BACK_SPACE);
         $("[data-test-id='date'] .input__control").sendKeys("25.01.2023");
         $("[data-test-id='name'] .input__control").sendKeys("Иван Иванов");
         $("[data-test-id='phone'] .input__control").sendKeys("+79998887766");
         $("[data-test-id='agreement']").click();
         $(byText("Забронировать")).click();
-        String text = $("[data-test-id='date'] .input_invalid .input__sub").getText();
-        assertEquals("Заказ на выбранную дату невозможен", text);
+        $("[data-test-id='date'] .input_invalid .input__sub").shouldHave(Condition.text("Заказ на выбранную дату невозможен"));
     }
 
     @Test
@@ -91,14 +95,12 @@ class DeliveryTest {
         open("http://localhost:9999/");
         $("[data-test-id='city'] .input__control").sendKeys("Ростов-на-Дону");
         $("[data-test-id='date'] .input__control").doubleClick();
-        $("[data-test-id='date'] .input__control").sendKeys(" ");
-        $("[data-test-id='date'] .input__control").sendKeys(" ");
+        $("[data-test-id='date'] .input__control").sendKeys(Keys.BACK_SPACE);
         $("[data-test-id='name'] .input__control").sendKeys("Иван Иванов");
         $("[data-test-id='phone'] .input__control").sendKeys("+79998887766");
         $("[data-test-id='agreement']").click();
         $(byText("Забронировать")).click();
-        String text = $("[data-test-id='date'] .input_invalid .input__sub").getText();
-        assertEquals("Неверно введена дата", text);
+        $("[data-test-id='date'] .input_invalid .input__sub").shouldHave(Condition.text("Неверно введена дата"));
     }
 
     @Test
@@ -110,14 +112,13 @@ class DeliveryTest {
         open("http://localhost:9999/");
         $("[data-test-id='city'] .input__control").sendKeys("Ростов-на-Дону");
         $("[data-test-id='date'] .input__control").doubleClick();
-        $("[data-test-id='date'] .input__control").sendKeys(" ");
-        $("[data-test-id='date'] .input__control").sendKeys(formattedDate);
+        $("[data-test-id='date'] .input__control").sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id='date'] .input__control").sendKeys(addDays(3));
         $("[data-test-id='name'] .input__control").sendKeys("Жанна д`Арк");
         $("[data-test-id='phone'] .input__control").sendKeys("+79998887766");
         $("[data-test-id='agreement']").click();
         $(byText("Забронировать")).click();
-        String text = $("[data-test-id='name'].input_invalid .input__sub").getText();
-        assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.", text);
+        $("[data-test-id='name'].input_invalid .input__sub").shouldHave(Condition.text("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
     }
 
     @Test
@@ -129,14 +130,13 @@ class DeliveryTest {
         open("http://localhost:9999/");
         $("[data-test-id='city'] .input__control").sendKeys("Ростов-на-Дону");
         $("[data-test-id='date'] .input__control").doubleClick();
-        $("[data-test-id='date'] .input__control").sendKeys(" ");
-        $("[data-test-id='date'] .input__control").sendKeys(formattedDate);
+        $("[data-test-id='date'] .input__control").sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id='date'] .input__control").sendKeys(addDays(3));
         $("[data-test-id='name'] .input__control").sendKeys(" ");
         $("[data-test-id='phone'] .input__control").sendKeys("+79998887766");
         $("[data-test-id='agreement']").click();
         $(byText("Забронировать")).click();
-        String text = $("[data-test-id='name'].input_invalid .input__sub").getText();
-        assertEquals("Поле обязательно для заполнения", text);
+        $("[data-test-id='name'].input_invalid .input__sub").shouldHave(Condition.text("Поле обязательно для заполнения"));
     }
 
     @Test
@@ -148,14 +148,13 @@ class DeliveryTest {
         open("http://localhost:9999/");
         $("[data-test-id='city'] .input__control").sendKeys("Ростов-на-Дону");
         $("[data-test-id='date'] .input__control").doubleClick();
-        $("[data-test-id='date'] .input__control").sendKeys(" ");
-        $("[data-test-id='date'] .input__control").sendKeys(formattedDate);
+        $("[data-test-id='date'] .input__control").sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id='date'] .input__control").sendKeys(addDays(3));
         $("[data-test-id='name'] .input__control").sendKeys("Иван Иванов");
         $("[data-test-id='phone'] .input__control").sendKeys("89999888776");
         $("[data-test-id='agreement']").click();
         $(byText("Забронировать")).click();
-        String text = $("[data-test-id='phone'].input_invalid .input__sub").getText();
-        assertEquals("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.", text);
+        $("[data-test-id='phone'].input_invalid .input__sub").shouldHave(Condition.text("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
     }
 
     @Test
@@ -167,14 +166,13 @@ class DeliveryTest {
         open("http://localhost:9999/");
         $("[data-test-id='city'] .input__control").sendKeys("Ростов-на-Дону");
         $("[data-test-id='date'] .input__control").doubleClick();
-        $("[data-test-id='date'] .input__control").sendKeys(" ");
-        $("[data-test-id='date'] .input__control").sendKeys(formattedDate);
+        $("[data-test-id='date'] .input__control").sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id='date'] .input__control").sendKeys(addDays(3));
         $("[data-test-id='name'] .input__control").sendKeys("Иван Иванов");
         $("[data-test-id='phone'] .input__control").sendKeys(" ");
         $("[data-test-id='agreement']").click();
         $(byText("Забронировать")).click();
-        String text = $("[data-test-id='phone'].input_invalid .input__sub").getText();
-        assertEquals("Поле обязательно для заполнения", text);
+        $("[data-test-id='phone'].input_invalid .input__sub").shouldHave(Condition.text("Поле обязательно для заполнения"));
     }
 
     @Test
@@ -186,12 +184,11 @@ class DeliveryTest {
         open("http://localhost:9999/");
         $("[data-test-id='city'] .input__control").sendKeys("Ростов-на-Дону");
         $("[data-test-id='date'] .input__control").doubleClick();
-        $("[data-test-id='date'] .input__control").sendKeys(" ");
-        $("[data-test-id='date'] .input__control").sendKeys(formattedDate);
+        $("[data-test-id='date'] .input__control").sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id='date'] .input__control").sendKeys(addDays(3));
         $("[data-test-id='name'] .input__control").sendKeys("Иван Иванов");
         $("[data-test-id='phone'] .input__control").sendKeys("+79999888776");
         $(byText("Забронировать")).click();
-        boolean checkbox = $("[data-test-id='agreement'].input_invalid").isDisplayed();
-        assertTrue(checkbox);
+        $("[data-test-id='agreement']").shouldNotBe(Condition.selected);
     }
 }
